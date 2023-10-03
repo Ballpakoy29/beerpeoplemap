88bdc60ea47c9d900fb852cd3b85a9903a4ab1d1
Nattawut Singkwang <Nattawut29.sin@gmail.com>
2023-10-04 04:48:00 +0700

    fix

--- src/components/beer.jsx
+++ src/components/beer.jsx
@@ -1,36 +1,35 @@
 import React from 'react';
 import { Card } from 'antd';
 import Typography from '@mui/material/Typography';
-// import PASUTARA from './../assets/PASUTARA.jpg';
-// import TENA from './../assets/TENA.jpg';
-// import TENAS from './../assets/TENAS.jpg';
-//import BARISTO from './../assets/BARISTO.jpg';
-import imgPromotion from './../assets/promotion2.png';
-// import RAINBOW from './../assets/RAINBOW.jpg';
-// import สุราข้าวหอมชัยภูมิ from './../assets/สุราข้าวหอมชัยภูมิ.jpg';
-// import อีสานรัม from './../assets/อีสานรัม.jpg';
-// import WHITE_SHARK_SUMMER_BEACH from './../assets/WHITE_SHARK_SUMMER_BEACH.jpg';
-// import โคโยตี้_ลำก้า from './../assets/โคโยตี้_ลำก้า.jpg';
-// import PUR_SPIRIT_OF_THAILAND from './../assets/PUR_SPIRIT_OF_THAILAND.jpg';
-// import SIAM_GIN from './../assets/SIAM_GIN.jpg';
-// import ERAWAN from './../assets/ERAWAN.jpg';
+import PASUTARA from './../assets/PASUTARA.jpg';
+import TENA from './../assets/TENA.jpg';
+import TENAS from './../assets/TENAS.jpg';
+import BARISTO from './../assets/BARISTO.jpg';
+import RAINBOW from './../assets/RAINBOW.jpg';
+import สุราข้าวหอมชัยภูมิ from './../assets/สุราข้าวหอมชัยภูมิ.jpg';
+import อีสานรัม from './../assets/อีสานรัม.jpg';
+import WHITE_SHARK_SUMMER_BEACH from './../assets/WHITE_SHARK_SUMMER_BEACH.jpg';
+import โคโยตี้_ลำก้า from './../assets/โคโยตี้_ลำก้า.jpg';
+import PUR_SPIRIT_OF_THAILAND from './../assets/PUR_SPIRIT_OF_THAILAND.jpg';
+import SIAM_GIN from './../assets/SIAM_GIN.jpg';
+import ERAWAN from './../assets/ERAWAN.jpg';
 
 const { Meta } = Card;
 
-// const imageMap = {
-//   'PASUTARA': PASUTARA,
-//   'TENA': TENA,
-//   'TENAS': TENAS,
-//   'BARISTO': BARISTO,
-//   'RAINBOW': RAINBOW,
-//   'สุราข้าวหอมชัยภูมิ' : สุราข้าวหอมชัยภูมิ,
-//   'อีสานรัม' : อีสานรัม,
-//   'WHITE_SHARK_SUMMER_BEACH' : WHITE_SHARK_SUMMER_BEACH,
-//   โคโยตี้_ลำก้า : โคโยตี้_ลำก้า,
-//   PUR_SPIRIT_OF_THAILAND : PUR_SPIRIT_OF_THAILAND,
-//   SIAM_GIN : SIAM_GIN,
-//   ERAWAN : ERAWAN
-// };
+const imageMap = {
+  'PASUTARA': PASUTARA,
+  'TENA': TENA,
+  'TENAS': TENAS,
+  'BARISTO': BARISTO,
+  'RAINBOW': RAINBOW,
+  'สุราข้าวหอมชัยภูมิ' : สุราข้าวหอมชัยภูมิ,
+  'อีสานรัม' : อีสานรัม,
+  'WHITE_SHARK_SUMMER_BEACH' : WHITE_SHARK_SUMMER_BEACH,
+  โคโยตี้_ลำก้า : โคโยตี้_ลำก้า,
+  PUR_SPIRIT_OF_THAILAND : PUR_SPIRIT_OF_THAILAND,
+  SIAM_GIN : SIAM_GIN,
+  ERAWAN : ERAWAN
+};
 
 const Beer = (props) => {
   const imageSrc = imageMap[props.items.brands.replace(/\s+/g, '_')];
@@ -39,7 +38,7 @@ const Beer = (props) => {
       <Card
         hoverable
         style={{ width: 240 }}
-        cover={<img alt="example" src={imgPromotion} />}
+        cover={<img alt="example" src={imageSrc} />}
       >
         <Meta title={`[${props.items.booth_id}] - ${props.items.brands}`} description={`[${props.items.booth_id}] - ${props.items.brands}`} />
       </Card>
