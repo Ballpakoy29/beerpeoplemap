07f7294d804708b5d42fadd11b2a1a3b995fee47
Nattawut Singkwang <Nattawut29.sin@gmail.com>
2023-10-04 23:10:39 +0700

    add pic

--- src/components/beer.jsx
+++ src/components/beer.jsx
@@ -5,46 +5,163 @@ import Typography from '@mui/material/Typography';
 import a1_2 from './../assets/a1_2.png'
 import a1_3 from './../assets/a1_3.png';
 import a1_1 from './../assets/a1_1.png';
-
 import a2_1 from './../assets/a2_1.png';
 import a2_2 from './../assets/a2_2.png';
-
 import a3_1 from './../assets/a3_1.png';
 import a3_2 from './../assets/a3_2.png';
 import a3_3 from './../assets/a3_3.png';
-
 import a4_1 from './../assets/a4_1.png';
-
 import a5_1 from './../assets/a5_1.png';
 import a5_2 from './../assets/a5_2.png';
 import a5_3 from './../assets/a5_3.png';
-
 import a6_1 from './../assets/a6_1.png';
 import a6_2 from './../assets/a6_2.png';
-
 import a7_1 from './../assets/a7_1.png';
 import a7_2 from './../assets/a7_2.png';
-
 import a8_1 from './../assets/a8_1.png';
 import a8_2 from './../assets/a8_2.png';
 import a8_3 from './../assets/a8_3.png';
-
 import a9_1 from './../assets/a9_1.png';
 import a9_2 from './../assets/a9_2.png';
 import a9_3 from './../assets/a9_3.png';
-
 import a10_1 from './../assets/a10_1.png';
 import a10_2 from './../assets/a10_2.png';
-
 import a11_1 from './../assets/a11_1.png';
 import a11_2 from './../assets/a11_2.png';
-
 import a12_1 from './../assets/a12_1.png';
 import a12_2 from './../assets/a12_2.png';
-
 import a13_1 from './../assets/a13_1.png';
 import a13_2 from './../assets/a13_2.png';
 import a13_3 from './../assets/a13_3.png';
+
+import b1_1 from './../assets/b1_1.png';
+import b1_2 from './../assets/b1_2.png';
+import b1_3 from './../assets/b1_3.png';
+import b1_4 from './../assets/b1_4.png';
+import b1_5 from './../assets/b1_5.png';
+import b1_6 from './../assets/b1_6.png';
+
+import b2_1 from './../assets/b2_1.png';
+import b2_2 from './../assets/b2_2.png';
+import b2_3 from './../assets/b2_3.png';
+
+import b3_1 from './../assets/b3_1.png';
+import b3_2 from './../assets/b3_2.png';
+import b3_3 from './../assets/b3_3.png';
+
+import b4_1 from './../assets/b4_1.png';
+import b4_2 from './../assets/b4_2.png';
+
+import b5_1 from './../assets/b5_1.png';
+
+import b6_1 from './../assets/b6_1.png';
+import b6_2 from './../assets/b6_2.png';
+import b6_3 from './../assets/b6_3.png';
+import b6_4 from './../assets/b6_4.png';
+import b6_5 from './../assets/b6_5.png';
+import b6_6 from './../assets/b6_6.png';
+
+import b7_1 from './../assets/b7_1.png';
+import b7_2 from './../assets/b7_2.png';
+import b7_3 from './../assets/b7_3.png';
+import b7_4 from './../assets/b7_4.png';
+import b7_5 from './../assets/b7_5.png';
+import b7_6 from './../assets/b7_6.png';
+
+import b8_1 from './../assets/b8_1.png';
+import b8_2 from './../assets/b8_2.png';
+
+import b9_1 from './../assets/b9_1.png';
+import b9_2 from './../assets/b9_2.png';
+
+import b10_1 from './../assets/b10_1.png';
+import b10_2 from './../assets/b10_2.png';
+
+import b11_1 from './../assets/b11_1.png';
+import b11_2 from './../assets/b11_2.png';
+
+import b12_1 from './../assets/b12_1.png';
+import b12_2 from './../assets/b12_2.png';
+
+import b13_1 from './../assets/b13_1.png';
+import b13_2 from './../assets/b13_2.png';
+
+import b14_1 from './../assets/b14_1.png';
+
+import b15_1 from './../assets/b15_1.png';
+import b15_2 from './../assets/b15_2.png';
+import b15_3 from './../assets/b15_3.png';
+
+import b16_1 from './../assets/b16_1.png';
+import b16_2 from './../assets/b16_2.png';
+
+import b17_1 from './../assets/b17_1.png';
+import b17_2 from './../assets/b17_2.png';
+
+import b18_1 from './../assets/b18_1.png';
+
+import b19_1 from './../assets/b19_1.png';
+import b19_2 from './../assets/b19_2.png';
+
+import b20_1 from './../assets/b20_1.png';
+import b20_2 from './../assets/b20_2.png';
+import b20_3 from './../assets/b20_3.png';
+
+import b21_1 from './../assets/b21_1.png';
+import b21_2 from './../assets/b21_2.png';
+import b21_3 from './../assets/b21_3.png';
+
+import b22_1 from './../assets/b22_1.png';
+import b22_2 from './../assets/b22_2.png';
+
+import b23_1 from './../assets/b23_1.png';
+
+import b24_1 from './../assets/b24_1.png';
+import b24_2 from './../assets/b24_2.png';
+import b24_3 from './../assets/b24_3.png';
+
+import b25_1 from './../assets/b25_1.png';
+import b25_2 from './../assets/b25_2.png';
+
+import b26_1 from './../assets/b26_1.png';
+import b26_2 from './../assets/b26_2.png';
+
+import b27_1 from './../assets/b27_1.png';
+import b27_2 from './../assets/b27_2.png';
+
+import b28_1 from './../assets/b28_1.png';
+import b28_2 from './../assets/b28_2.png';
+
+import b29_1 from './../assets/b29_1.png';
+import b29_2 from './../assets/b29_2.png';
+import b29_3 from './../assets/b29_3.png';
+
+import b30_1 from './../assets/b30_1.png';
+import b30_2 from './../assets/b30_2.png';
+
+import b31_1 from './../assets/b31_1.png';
+import b31_2 from './../assets/b31_2.png';
+import b31_3 from './../assets/b31_3.png';
+
+import b32_1 from './../assets/b32_1.png';
+import b32_2 from './../assets/b32_2.png';
+
+import b33_1 from './../assets/b33_1.png';
+
+import b34_1 from './../assets/b34_1.png';
+import b34_2 from './../assets/b34_2.png';
+
+import b35_1 from './../assets/b35_1.png';
+import b35_2 from './../assets/b35_2.png';
+import b35_3 from './../assets/b35_3.png';
+
+import b36_1 from './../assets/b36_1.png';
+import b36_2 from './../assets/b36_2.png';
+import b36_3 from './../assets/b36_3.png';
+
+import b37_1 from './../assets/b37_1.png';
+import b37_2 from './../assets/b37_2.png';
+import b37_3 from './../assets/b37_3.png';
 
 const { Meta } = Card;
 
@@ -92,6 +209,135 @@ const imageMap = {
   'INTHONE_BREWING': a13_1,
   'HOPSTER': a13_2,
   'CHONG': a13_3,
+
+  'โฉมฉาย': b1_1,
+  'สละโสด': b1_2,
+  'JELLYGEMS': b1_3,
+  'THE_GUDNER_BREWING': b1_4,
+  'TRIPLE_PEARL': b1_5,
+  'ดิษฐสละ': b1_6,
+
+  'CALL_ME_PAPA': b2_1,
+  'เชียงใหม่เบียร์': b2_2,
+  'KWAI_BEER': b2_3,
+
+  'SAMATA': b3_1,
+  'THE_BREWING_PROJECT': b3_2,
+  'WIZARD': b3_3,
+
+  'KHOY_BREWING': b4_1,
+  'DADDY_DOG': b4_2,
+
+  'SUNTREE_BREWING': b5_1,
+
+  'REPENTLESS_BREWING': b6_1,
+  'DYING_BRAIN': b6_2,
+  'H-SHOT': b6_3,
+  'MAX_BEER': b6_4,
+  'NICE_CIDER': b6_5,
+  'YOD_BEER': b6_6,
+
+  'จัณฑ\'ลา': b7_1,
+  'BALDER_BRAVE_BREWING': b7_2,
+  'ปลาตีน_CRAFT_BEER': b7_3,
+  'CHACHOENGSAO_FARM': b7_4,
+  'SAIPON_BREWING': b7_5,
+  'BANGWUA_BREWING': b7_6,
+
+  'MIDLAND': b8_1,
+  'ONE_FOR_THE_ROAD_BREWING': b8_2,
+
+  'JIMBREW': b9_1,
+  'RED_DUCK_ROCKET': b9_2,
+
+  'KIM’S_BREWING': b10_1,
+  'YOURBREWING': b10_2,
+
+  'HOPPYLISM': b11_1,
+  'CRIMINAL': b11_2,
+
+  'ลมจอยบรูวอิ้ง': b12_1,
+  'ทองดีคราฟท์': b12_2,
+
+  'AUNDER_WILD_BREWING': b13_1,
+  '27/7': b13_2,
+
+  'COLD_CAT': b14_1,
+
+  'TRAIN_CRAFT_BEER': b15_1,
+  'KHONWAN_BREWING': b15_2,
+  'RECONNECT_CRAFT': b15_3,
+
+  'MUSINGS_ERROR': b16_1,
+  'คีรี_RAYONG_HOMEBREW': b16_2,
+
+  'SANG_HOK': b17_1,
+  'วรรณา': b17_2,
+
+  'RINN_BREWING': b18_1,
+
+  'WARIN_BREW': b19_1,
+  'SONTAYA_BREWING': b19_2,
+
+  'SUNRISE': b20_1,
+  'ZILALOM': b20_2,
+  'บางปลาสร้อย': b20_3,
+
+  'KUMAGASM': b21_1,
+  'HAPLAB': b21_2,
+  'LAZY_FAT_CAT': b21_3,
+
+  'สาธุ_99': b22_1,
+  'ASHI_เชียงดาว': b22_2,
+
+  'DEVANOM': b23_1,
+
+  'WAKBREW': b24_1,
+  'BAN_CHANG_BOYS': b24_2,
+  'HIPPO_SLOW_CRAFT': b24_3,
+
+  '"HIGH_BRIDGE_BREWING': b25_1,
+  'GG_BREWERY': b25_2,
+
+  'ELEPHANTS_IN_THE_POOL': b26_1,
+  'BACKYARD_BREWERY': b26_2,
+
+  '96_BREWING': b27_1,
+  '25_กุมภา': b27_2,
+
+  'WHITE_CAT': b28_1,
+  'BREW_BOR_AI': b28_2,
+
+  'U_ZERO': b29_1,
+  'BREW_RABBIT': b29_2,
+  'SANTI_SODA': b29_3,
+
+  'MONGKOL_BREWING': b30_1,
+  'FAT_CAT_62_BREWING': b30_2,
+
+  'SAMILA': b31_1,
+  'FISHBRIDGE_BREWING': b31_2,
+  'NIMITMAI_BREWING': b31_3,
+
+  'MISS_ME': b32_1,
+  'HOUSE_OF_LADY': b32_2,
+
+  'WE_NEED_TO_TALK': b33_1,
+
+  'TOO_BREW': b34_1,
+  'HEADKHE': b34_2,
+
+  'SUN_STRIKE_BREWING': b35_1,
+  'FOUR_JACKS_BREWING': b35_2,
+  'PRADIT': b35_3,
+
+  'เหมียวสมบูรณ์': b36_1,
+  'GE-NIUS': b36_2,
+  'VR.MOX': b36_3,
+
+  'OVERNIGHT': b37_1,
+  'MAD_BEER': b37_2,
+  'โกดังลับ': b37_3,
 
 };
 
