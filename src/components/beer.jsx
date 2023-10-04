a9af39fccb56a4cc2aa4cdd8dd3e477a19a98bac
Nattawut Singkwang <Nattawut29.sin@gmail.com>
2023-10-04 19:09:57 +0700

    test img

--- src/components/beer.jsx
+++ src/components/beer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card } from 'antd';
 import Typography from '@mui/material/Typography';
-// import PASUTARA from './../assets/PASUTARA.jpg';
-// import TENA from './../assets/TENA.jpg';
-// import TENAS from './../assets/TENAS.jpg';
+import a1_2 from './../assets/a1_2.png'
+import a1_3 from './../assets/a1_3.png';
+import a1_1 from './../assets/a1_1.png';
 
-import RAINBOW from './../assets/rainbow.jpg';
-import imgPromotion from './../assets/promotion2.png';
+import a2_1 from './../assets/a2_1.png';
+import a2_2 from './../assets/a2_2.png';
+
 // import สุราข้าวหอมชัยภูมิ from './../assets/สุราข้าวหอมชัยภูมิ.jpg';
 // import อีสานรัม from './../assets/อีสานรัม.jpg';
 // import WHITE_SHARK_SUMMER_BEACH from './../assets/WHITE_SHARK_SUMMER_BEACH.jpg';
@@ -17,29 +18,31 @@ import imgPromotion from './../assets/promotion2.png';
 
 const { Meta } = Card;
 
-// const imageMap = {
-//   'PASUTARA': PASUTARA,
-//   'TENA': TENA,
-//   'TENAS': TENAS,
+const imageMap = {
+  'TENAS' : a1_1,
+  'PASUTARA': a1_2,
+  'TENA': a1_3,
+  'BARISTO': a2_1,
+  'RAINBOW': a2_2,
 
-//   'RAINBOW': RAINBOW,
-//   'สุราข้าวหอมชัยภูมิ' : สุราข้าวหอมชัยภูมิ,
-//   'อีสานรัม' : อีสานรัม,
-//   'WHITE_SHARK_SUMMER_BEACH' : WHITE_SHARK_SUMMER_BEACH,
-//   'โคโยตี้_ลำก้า' : โคโยตี้_ลำก้า,
-//   'PUR_SPIRIT_OF_THAILAND' : PUR_SPIRIT_OF_THAILAND,
-//   'SIAM_GIN' : SIAM_GIN,
-//   'ERAWAN' : ERAWAN
-// };
+
+  // 'สุราข้าวหอมชัยภูมิ' : สุราข้าวหอมชัยภูมิ,
+  // 'อีสานรัม' : อีสานรัม,
+  // 'WHITE_SHARK_SUMMER_BEACH' : WHITE_SHARK_SUMMER_BEACH,
+  // 'โคโยตี้_ลำก้า' : โคโยตี้_ลำก้า,
+  // 'PUR_SPIRIT_OF_THAILAND' : PUR_SPIRIT_OF_THAILAND,
+  // 'SIAM_GIN' : SIAM_GIN,
+  // 'ERAWAN' : ERAWAN
+};
 
 const Beer = (props) => {
-  //const imageSrc = imageMap[props.items.brands.replace(/\s+/g, '_')];
+  const imageSrc = imageMap[props.items.brands.replace(/\s+/g, '_')];
   return (
     <div>
       <Card
         hoverable
         style={{ width: 240 }}
-        cover={<img alt="example" src={RAINBOW} />}
+        cover={<img alt="example" src={imageSrc} />}
       >
         <Meta title={`[${props.items.booth_id}] - ${props.items.brands}`} description={`[${props.items.booth_id}] - ${props.items.brands}`} />
       </Card>
